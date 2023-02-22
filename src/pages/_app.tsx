42177f1c0022920d94f11b5676f86446c08c2aa2
Joelson Dias <joelsondiasti@outlook.com>
2023-02-21 23:29:12 -0300

    Start of project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { globalStyles } from "@/styles/global";
+import type { AppProps } from "next/app";
+import logoImg from "../assets/logo.svg";
+import { Container, Header } from "@/styles/pages/app";
+import Image from "next/image";
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <Image src={logoImg.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { getCssText } from "@/styles";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@500;700&display=swap"
+          rel="stylesheet"
+        />
+
+        <style id="stitches" dangerouslySetInnerHTML={{__html: getCssText()}}/>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
